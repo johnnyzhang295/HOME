1298f5e10687897a964ef11055df17d1f497b729
jz_biopac <BIOPACMan@local.edu>
2020-10-08 07:31:41 -0600

    add rmssd plots

--- plotting/HRV_plot_indiv.m
+++ plotting/HRV_plot_indiv.m
@@ -21,57 +21,95 @@ HFn_211 = p211(:,8);
 HFn_213 = p213(:,8);
 
 xfit = linspace(.65,1);
+subplot(2,3,1)
+scatter(HFn_201,wl_201,20,'r','filled','DisplayName','Part 201')
+p = polyfit(HFn_201,wl_201,1);
+hold on
+xfit = linspace(.65,1);
+yfit = polyval(p,xfit);
+plot(xfit,yfit,'r','HandleVisibility','off');
+hold off
+legend('Location','northwest')
+xlabel('Normalized HF')
+ylabel('Subjective Workload Rating')
+grid on
+grid minor
 
-figure;
+
+subplot(2,3,2)
 hold on;
-scatter(HFn_202,wl_202,80,'b','filled','DisplayName','Part 202')
+scatter(HFn_202,wl_202,20,'b','filled','DisplayName','Part 202')
 p2 = polyfit(HFn_202,wl_202,1);
 yfit2 = polyval(p2,xfit);
 plot(xfit,yfit2,'b','HandleVisibility','off');
+hold off
 legend('Location','northwest')
 xlabel('Normalized HF')
 ylabel('Subjective Workload Rating')
-title('Workload Rating vs Normalized HF')
-xlim([.9,1])
-ylim([1,6])
-yticks(1:1:6)
 grid on
 grid minor
-hold off;
+xlim([.9,1])
+ylim([1,6])
 
-
-figure;
+subplot(2,3,3)
 hold on;
-scatter(HFn_208,wl_208,80,'c','filled','DisplayName','Part 208')
+scatter(HFn_203,wl_203,20,'m','filled','DisplayName','Part 203')
+p3 = polyfit(HFn_203,wl_203,1);
+yfit3 = polyval(p3,xfit);
+plot(xfit,yfit3,'m','HandleVisibility','off');
+hold off
+xlim([.9,1])
+ylim([1,6])
+legend('Location','northwest')
+xlabel('Normalized HF')
+ylabel('Subjective Workload Rating')
+grid on
+xlim([.85,1])
+ylim([2,8])
+grid minor
+
+subplot(2,3,4)
+hold on;
+scatter(HFn_208,wl_208,20,'c','filled','DisplayName','Part 208')
 p4 = polyfit(HFn_208,wl_208,1);
 yfit4 = polyval(p4,xfit);
 plot(xfit,yfit4,'c','HandleVisibility','off');
-
+hold off
 legend('Location','northwest')
 xlabel('Normalized HF')
 ylabel('Subjective Workload Rating')
-title('Workload Rating vs Normalized HF')
-xlim([.75,1])
-ylim([2,10])
-yticks(2:1:10)
 grid on
 grid minor
-hold off;
+xlim([.7,1])
+ylim([2,10])
 
-figure;
+subplot(2,3,5)
 hold on;
-scatter(HFn_213,wl_213,80,'g','filled','DisplayName','Part 213')
+scatter(HFn_211,wl_211,20,'k','filled','DisplayName','Part 211')
+p5 = polyfit(HFn_211,wl_211,1);
+yfit5 = polyval(p5,xfit);
+plot(xfit,yfit5,'k','HandleVisibility','off');
+hold off
+legend('Location','northwest')
+xlabel('Normalized HF')
+ylabel('Subjective Workload Rating')
+grid on
+grid minor
+xlim([.7,1])
+ylim([0,8])
+
+subplot(2,3,6)
+hold on;
+scatter(HFn_213,wl_213,20,'g','filled','DisplayName','Part 213')
 p6 = polyfit(HFn_213,wl_213,1);
 yfit6 = polyval(p6,xfit);
 plot(xfit,yfit6,'g','HandleVisibility','off');
+hold off
+xlim([.8,1])
+ylim([2,8])
 
 legend('Location','northwest')
 xlabel('Normalized HF')
 ylabel('Subjective Workload Rating')
-title('Workload Rating vs Normalized HF')
-xlim([.8,1])
-ylim([2,8])
-yticks(2:1:8)
 grid on
 grid minor
-hold off;
